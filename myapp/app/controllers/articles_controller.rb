28b485329c218c05582f2759fa8e116a85e3ec90
Fatma <fatma.tarek204@gmail.com>
2021-05-09 17:47:37 +0200

    add validation to forms

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     def new
-        @name = params["name"]
+        #@name = params["name"]
+        @article = Article.new
     end
 
     # def create
@@ -17,8 +18,12 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.save
-        redirect_to @article
+ 
+        if @article.save
+          redirect_to @article
+        else
+          render 'new'
+        end
         end
 
     def show
