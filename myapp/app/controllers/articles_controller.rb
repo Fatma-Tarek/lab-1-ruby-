582e4f340d0a03d61cb136424d6c45a139d19354
Fatma <fatma.tarek204@gmail.com>
2021-05-09 17:09:19 +0200

    save form successfully

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -3,9 +3,31 @@ class ArticlesController < ApplicationController
         @name = params["name"]
     end
 
+    # def create
+    #     byebug
+    #     ## ActiveModel::ForbiddenAttributesError
+    #     # @article = Article.new(params[:article]) #create instance of variable    
+    #     @article = Article.new(params.require(:article).permit(:title, :text))
+    #     @article.save
+    #     redirect_to @article
+    # end
+    
     def create
-       # byebug
-       render plain: params[:article].inspect
-    end
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to @article
+        end
+        
+    def show
+        @article = Article.find(params[:id])
+     end
+
+    private
+        #called when create anything
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+
+        
 end
 
