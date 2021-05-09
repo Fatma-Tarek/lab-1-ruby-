05ba1413cf73a1cdea9fc418a7caeb78595fc18f
Fatma <fatma.tarek204@gmail.com>
2021-05-09 17:12:49 +0200

    listing all articles

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -11,13 +11,16 @@ class ArticlesController < ApplicationController
     #     @article.save
     #     redirect_to @article
     # end
-    
+    def index
+        @articles = Article.all
+      end
+
     def create
         @article = Article.new(article_params)
         @article.save
         redirect_to @article
         end
-        
+
     def show
         @article = Article.find(params[:id])
      end
