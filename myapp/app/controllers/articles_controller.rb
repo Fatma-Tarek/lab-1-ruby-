24a8ecd761edd31b17bb9c19d056edf2c0ce781d
Fatma <fatma.tarek204@gmail.com>
2021-05-09 18:03:02 +0200

    update the articles

--- myapp/app/controllers/articles_controller.rb
+++ myapp/app/controllers/articles_controller.rb
@@ -25,6 +25,20 @@ class ArticlesController < ApplicationController
           render 'new'
         end
         end
+    
+    def update
+            @article = Article.find(params[:id])
+           
+            if @article.update(article_params)
+              redirect_to @article
+            else
+              render 'edit'
+            end
+        end
+
+    def edit
+        @article = Article.find(params[:id])
+     end
 
     def show
         @article = Article.find(params[:id])
